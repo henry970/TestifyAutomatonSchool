6d2ae127d1fe75164df98b140428b1250ac41894
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2022-10-15 21:27:00 +0100

    update

---
+++ JsCodingTask/Challenge10.js
@@ -0,0 +1,8 @@
+
+// Task_10
+// Create  a  function  that  filters  out  negative numbers
+
+
+let negativeNumber = [31, -32, 33, 34, -35, -36, 37, 38, 39, 40, -41, -42]
+array = negativeNumber.filter(function(n) { return n < -1; })
+console.log(array)
---
+++ JsCodingTask/Challenge6.js
@@ -0,0 +1,9 @@
+
+// Task_6
+// Sort an array of strings in alphabetical order
+
+
+const array = ["Peace","Princess", "Henry", "Yekinni", "Okolie", "Obi", "Chijioke", "Ibironke", "Onyedikachi"];
+ array.sort();
+
+ console.log(array)
