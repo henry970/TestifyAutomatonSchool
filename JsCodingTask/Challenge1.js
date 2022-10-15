0e05fe407096e413074d1b49a38120f38556fcde
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2022-10-15 13:23:40 +0100

    update

--- JsCodingTask/Challenge1.js
+++ JsCodingTask/Challenge1.js
@@ -1,1 +1,8 @@
-console("hello world")
+//Task
+
+let numbers = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i]
+}
+console.log(sum)
