b4d89cb0ca3e98645e76cd1d7c44378ab70b53c5
henry970 <60664170+henry970@users.noreply.github.com>
2022-10-27 16:46:36 +0100

    Update location_strategy_id_classname_name.py

--- selenium_web/location_strategy_id_classname_name.py
+++ selenium_web/location_strategy_id_classname_name.py
@@ -4,19 +4,37 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def locate_by_id(driver):
-    pass
+    email_element = driver.find_element(By.ID, "email")
+    print("Element", email_element)
+    password_element = driver.find_element(By.ID, "pass")
+    print("Element", password_element)
 
 
 def locate_by_name(driver):
-    pass
+    firstname_element = driver.find_element(By.NAME, "firstname")
+    print("FristnameElement", firstname_element)
+    lastname_element = driver.find_element(By.NAME, "lastname")
+    print("Lastname Element", lastname_element)
 
 
 
 def locate_by_class_name(driver):
-    pass
+    rr_first_element = driver.find_element(By.CLASS_NAME, "react-reveal")
+    print("React Reveal First Element", rr_first_element)
+    rr_elements = driver.find_elements(By.CLASS_NAME, "react-reveal")
+    for rr_element in rr_elements:
+        print("React Reveal Elements", rr_element)
 
 
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://www.testifyltd.com/")
-    locate_by_id()
+    driver.get("https://www.testifyltd.com/contact")
+    # locate_by_id(driver)
+    # locate_by_name(driver)
+    locate_by_class_name(driver)
+
+
+
+
+if __name__ == '__main__':
+    main()
