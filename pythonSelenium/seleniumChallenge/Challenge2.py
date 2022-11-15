92f97dcf7f44afa9bff266ff8bfeaefaccc85000
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2022-11-15 17:32:53 +0100

    update challenge

--- pythonSelenium/seleniumChallenge/Challenge2.py
+++ pythonSelenium/seleniumChallenge/Challenge2.py
@@ -18,8 +18,8 @@ def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.google.com/")
     send_keys_to_google(driver.find_element(By.XPATH, '//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]'), "Python")
-    loginButton = driver.find_element(By.XPATH, "//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[4]/center[1]/input[1]").click()
-    print("login:", loginButton)
+    searchbox = driver.find_element(By.XPATH, "//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[4]/center[1]/input[1]").click()
+    print("login:", searchbox)
 
     text_on_wikipedia = driver.find_element(By.XPATH, "//span[contains(text(),'Python is a high-level, general-purpose programmin')]")
     print("Wikipedia:", text_on_wikipedia.text)
