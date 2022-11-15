d820d9e2165da5f03128061fcdbf4e9c82ec1b1b
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2022-11-15 17:40:25 +0100

    update challenge

--- pythonSelenium/seleniumChallenge/Challenge2.py
+++ pythonSelenium/seleniumChallenge/Challenge2.py
@@ -14,7 +14,7 @@ def send_keys_to_google(element, *keys):
     element.send_keys(keys)
 
 def main():
-   driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
     driver.get("https://www.google.com/")
     send_keys_to_google(driver.find_element(By.XPATH, '//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]'), "Python")
     searchbox = driver.find_element(By.XPATH, "//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[4]/center[1]/input[1]").click()
