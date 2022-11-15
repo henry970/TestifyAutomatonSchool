52bc5a51fad0b22b6a572628bc004483ec729ee7
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2022-11-15 17:39:47 +0100

    update challenge

--- pythonSelenium/seleniumChallenge/Challenge2.py
+++ pythonSelenium/seleniumChallenge/Challenge2.py
@@ -4,18 +4,17 @@
 # enter  the text  Python in the search  box, then  send  the  Enter  key.
 # Get  the text  from  the Wikipedia brief on the  right side and  print the  value in the console.
 
-import time
 from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
 
 def send_keys_to_google(element, *keys):
     element.send_keys(keys)
 
 def main():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+   driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
     driver.get("https://www.google.com/")
     send_keys_to_google(driver.find_element(By.XPATH, '//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]'), "Python")
     searchbox = driver.find_element(By.XPATH, "//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[4]/center[1]/input[1]").click()
